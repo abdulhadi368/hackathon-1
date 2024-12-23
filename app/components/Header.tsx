e21c910664bc86373a5452da8e397f818bb99764
abdulhadi368 <aminsamo3@gmail.com>
2024-12-23 22:58:28 +0500

    images

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,13 +9,13 @@ const Header = () => {
         <div className="h-[46px] w-[415px] gap-[10px] flex">
           <div className="h-[44px] w-[145px] flex gap-[10px] justify-center items-center">
             <div className="h-[16px] w-[16px]">
-              <Image src="/image/phone/icn settings icn-xs.svg" alt="Phone"  height={100} width={100}/>
+              <Image src="/images/phone/icn settings icn-xs.svg" alt="Phone"  height={100} width={100}/>
             </div>
                <h6 style={{ fontWeight: '700' }} className="h-[24px] w-[104px] text-[14px] text-[#FFFFFF] font-montserrat">(225) 555-0118</h6>
             </div>
           <div className="h-[44px] w-[260px] gap-[10px] flex justify-center items-center">
             <div className="h-[16px] w-[16px]">
-              <Image src="/image/message/icn settings icn-xs (1).svg" alt="Message" height={100} width={100}/>
+              <Image src="/images/message/icn settings icn-xs (1).svg" alt="Message" height={100} width={100}/>
             </div>
             <h6 style={{ fontWeight: '700' }} className="h-[24px] w-[219px] text-[14px] text-[#FAFAFA] font-montserrat">michelle.rivera@example.com</h6>
           </div>
@@ -26,10 +26,10 @@ const Header = () => {
         <div className="h-[46px] w-[233px] gap-[10px] flex items-center justify-center">
             <h6 style={{ fontWeight: '700' }} className="h-[24px] w-[83px] font-montserrat text-[14px] text-[#FAFAFA]">Follow Us :</h6>
           <div className="h-[26px] w-[120px] flex gap-[5px]">
-            <Image className="h-[26px] w-[26px]" src="/image/insta/a.svg" alt="insta"  height={100} width={100}/>
-            <Image className="h-[26px] w-[26px]" src="/image/youtube/a (1).svg" alt="yt" height={100} width={100}/>
-            <Image className="h-[26px] w-[26px]" src="/image/facebook/a (2).svg" alt="fb" height={100} width={100}/>
-            <Image className="h-[26px] w-[26px]" src="/image/twitter/a (3).svg" alt="twit"  height={100} width={100}/>
+            <Image className="h-[26px] w-[26px]" src="/images/insta/a.svg" alt="insta"  height={100} width={100}/>
+            <Image className="h-[26px] w-[26px]" src="/images/youtube/a (1).svg" alt="yt" height={100} width={100}/>
+            <Image className="h-[26px] w-[26px]" src="/images/facebook/a (2).svg" alt="fb" height={100} width={100}/>
+            <Image className="h-[26px] w-[26px]" src="/images/twitter/a (3).svg" alt="twit"  height={100} width={100}/>
           </div>
         </div>
       </div>
